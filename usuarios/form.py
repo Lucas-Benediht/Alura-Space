9545af73c40d93a1481e8bcad7ba150182151b07
Benee <lucas_benediht@hotmail.com>
2023-10-23 19:24:40 -0300

    Novas funcionalidades, cadastro, login, adicionar imagens

---
+++ usuarios/form.py
@@ -0,0 +1,16 @@
+# Criar objetos
+
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login=forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100
+    )
+    senha=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput()
+    )
